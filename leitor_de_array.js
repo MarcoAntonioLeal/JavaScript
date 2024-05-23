2075ac7fcba230795617064fcf78fe65402d0458
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2024-05-22 22:20:00 -0300

    Update leitor_de_array.js

--- leitor_de_array.js
+++ leitor_de_array.js
@@ -1,15 +1,3 @@
-/*console.log('Vamos começar a contar')
-
-let numero = 1
-
-const intervalId = setInterval(() => {
-    console.log(numero += 2)
-    if(numero > 12) {
-        clearInterval(intervalId)
-        console.log('Vamos parar por aqui')
-    }
-}, 1000 * 2)*/
-
 const dados = [
     { nome: 'Marco', idade: 34, cor: 'vermelha', cidade: 'Nova Friburgo', quantidade: 4, tempo: 2 },
     { nome: 'Sayara', idade: 34, cor: 'Azul', cidade: 'Cordeiro', quantidade: 2, tempo: 3 },
@@ -18,15 +6,42 @@ const dados = [
 
 let quantidade = 0 //valor fixo em 0
 
-const intervalId = setInterval(() => {
-    console.log(dados[0].nome)
-    quantidade += 1
+function array0() {
+    const intervalId = setInterval(() => {
+        console.log(dados[0].nome)
+        quantidade += 1
+    
+        if (quantidade == dados[0].quantidade) {
+            clearInterval(intervalId)
+            quantidade = 0
+            array1()
+        }
+    }, 1000 * dados[0].tempo)
+}
 
-    if (quantidade == dados[0].quantidade) {
-        clearInterval(intervalId)
-        console.log('olá, mundo!')
-    }
-}, 1000 * dados[0].tempo)
+function array1() {
+    const intervalId = setInterval(() => {
+        console.log(dados[1].nome)
+        quantidade += 1
+    
+        if (quantidade == dados[1].quantidade) {
+            clearInterval(intervalId)
+            
+        }
+    }, 1000 * dados[1].tempo)
+}
+
+function array2() {
+    const intervalId = setInterval(() => {
+        console.log(dados[2].nome)
+        quantidade += 1
+    
+        if (quantidade == dados[2].quantidade) {
+            clearInterval(intervalId)
+        }
+    }, 1000 * dados[2].tempo)
+}
 
 
 
+array0()
