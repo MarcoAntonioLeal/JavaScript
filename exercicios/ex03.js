c386ca680a1158b3831232a63f647799d6cbfdb2
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2024-07-01 22:13:05 -0300

    organizando arquivos

--- exercicios/ex03.js
+++
@@ -1,6 +0,0 @@
-//03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
-
-function potenciacao(a, b) {
-    console.log(a ** b)
-}
-potenciacao(2, 10)
--- exercicios/ex04.js
+++
@@ -1,7 +0,0 @@
-//04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado e o resto da divisão destes dois valores.
-
-function divisao(a, b) {
-    console.log (`A divisão de ${a} e ${b} é ${a / b}`)
-    console.log(`O resto da divisão de ${a} e ${b} é ${a % b}`)
-}
-divisao(30, 3)
