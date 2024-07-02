ce8dec5028b2e7cad37328219343f7e35ea83915
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2024-07-01 23:07:11 -0300

    exercício 04 concluído

---
+++ exercicios/exercicios_parte01/dist/exercicio04.js
@@ -0,0 +1,5 @@
+/*04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado e o resto da divisão destes dois valores.*/
+function dividindoValores(dividendo, divisor) {
+    console.log(`${dividendo} / ${divisor} = ${(dividendo / divisor).toFixed(2)}`);
+}
+dividindoValores(85, 7);
