7fac79194077f1a4081b8515c092b3aced910e4e
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2024-07-04 00:20:24 -0300

    exercício 02 concluído

---
+++ exercicios/exercicios_parte03/dist/exercicio02.js
@@ -0,0 +1,5 @@
+/*02) Escreva um programa que calcule a média aritmética de dois números.*/
+function mediaAritmetica(valor1, valor2) {
+    console.log((valor1 + valor2) / 2);
+}
+mediaAritmetica(8, 4);
---
+++ exercicios/exercicios_parte03/src/exercicio02.ts
@@ -0,0 +1,7 @@
+/*02) Escreva um programa que calcule a média aritmética de dois números.*/
+
+function mediaAritmetica(valor1:number, valor2:number) {
+    console.log((valor1 + valor2) / 2)
+}
+
+mediaAritmetica(8, 4)
