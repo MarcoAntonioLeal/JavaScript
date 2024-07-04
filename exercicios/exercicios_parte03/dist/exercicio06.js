e12b09bfe804ed564543eba5351d7117beb2a61c
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2024-07-04 01:08:24 -0300

    exercício 07 concluído

--- exercicios/exercicios_parte03/dist/exercicio06.js
+++ exercicios/exercicios_parte03/dist/exercicio06.js
@@ -1,5 +1,5 @@
 /*06) Crie um programa que calcule e exiba o perímetro de um círculo, solicitando o raio ao usuário. C = 2 * π * r*/
 function perimetroCirculo(raio) {
-    console.log((2 * raio * Math.PI).toFixed(2));
+    console.log(2 * raio * Math.PI);
 }
 perimetroCirculo(5);
