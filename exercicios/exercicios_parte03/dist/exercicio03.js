1e944ccfeb618ecec6ee2f1ea93fc6cfbc8755b9
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2024-07-04 00:23:01 -0300

    exercício 03 concluído

---
+++ exercicios/exercicios_parte03/dist/exercicio03.js
@@ -0,0 +1,5 @@
+/*03) Crie um programa que calcule e exiba a média aritmética de três notas informadas pelo usuário.*/
+function mediaAritmetica(valor1, valor2, valor3) {
+    console.log((valor1 + valor2 + valor3) / 3);
+}
+mediaAritmetica(2, 5, 8);
---
+++ exercicios/exercicios_parte03/src/exercicio03.ts
@@ -0,0 +1,7 @@
+/*03) Crie um programa que calcule e exiba a média aritmética de três notas informadas pelo usuário.*/
+
+function mediaAritmetica(valor1:number, valor2:number, valor3:number) {
+    console.log((valor1 + valor2 + valor3) / 3)
+}
+
+mediaAritmetica(2, 5, 8)
