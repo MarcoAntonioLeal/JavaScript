554978b6f859f51718ef39fd0b7ca9cabdda8ad2
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2024-07-05 23:15:42 -0300

    exercício 11 concluído

---
+++ exercicios/exercicios_parte03/src/exercicio11.ts
@@ -0,0 +1,7 @@
+/*11) Escreva um programa que calcule a velocidade média de um objeto, utilizando a fórmula v = Δs/Δt, onde v é a velocidade média, Δs é a variação de espaço e Δt é a variação de tempo.*/
+
+function velocidadeMedia(variacaoEspaco:number, variacaoTempo:number) {
+    console.log(variacaoEspaco / variacaoTempo)
+}
+
+velocidadeMedia(8, 4)
