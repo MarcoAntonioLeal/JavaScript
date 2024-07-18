dd72fea4df58cac6465169596fec31a73caa2a93
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2024-07-18 00:23:08 -0300

    exercício 02 concluído

---
+++ exercicios/exercicios_parte03/src/lacos_de_reticao/exercicio02.ts
@@ -0,0 +1,14 @@
+/*02) Escreva um programa que exiba todos os números de 1 a 10*/
+
+//Por while
+let i = 0
+
+while(i <= 100) {
+    console.log(i)
+    i++
+}
+
+//Por for
+for(let i = 0; i <= 100; i++) {
+    console.log(i)
+}
