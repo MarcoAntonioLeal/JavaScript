8aa7e8280b8132186da5b5a46bcd17bfbec5fb93
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2024-07-18 00:17:47 -0300

    exercício concluído

---
+++ exercicios/exercicios_parte03/dist/lacos_de_reticao/exercicio01.js
@@ -0,0 +1,11 @@
+/*01) Exiba os números de 01 a 10 utilizando os laços de repetição*/
+//Por while
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+//Por for
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
