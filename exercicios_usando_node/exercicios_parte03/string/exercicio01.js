4c2dcdc72c0a27ade4f11ce03a7b6b49fdd51775
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-01-04 21:17:34 -0300

    atualizando exercícios

--- exercicios_usando_node/exercicios_parte03/string/exercicio01.js
+++ exercicios_usando_node/exercicios_parte03/string/exercicio01.js
@@ -1,1 +1,8 @@
 /*Crie um programa que leia duas palavras e as concatene*/
+
+const primeiroNome = 'Marco Antonio'
+const segundoNome = 'Stanziola Leal Filho'
+
+console.log(primeiroNome + ' ' + segundoNome)
+console.log(`${primeiroNome} ${segundoNome}`)
+console.log(primeiroNome.concat(segundoNome))
--- exercicios_usando_node/exercicios_parte03/string/exercicio02.js
+++ exercicios_usando_node/exercicios_parte03/string/exercicio02.js
@@ -1,1 +1,10 @@
 /*Faça um programa que receba uma palavra e exiba cada letra separadamente*/
+
+const nome = 'Marco Antonio Stanziola Leal Filho'
+const novoNome = nome.replaceAll(' ', '')
+
+console.log(nome.replaceAll(' ', '').split(''))
+
+for(let i = 0; i < novoNome.length; i++) {
+    console.log(novoNome[i])
+}
