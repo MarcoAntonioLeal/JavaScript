bd942f267d7906d58cfe541b67a99bd338641a4f
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-01-07 20:38:27 -0300

    exercício 06 concluído

--- exercicios_usando_node/exercicios_parte03/string/exercicio06.js
+++ exercicios_usando_node/exercicios_parte03/string/exercicio06.js
@@ -1,1 +1,18 @@
 /*Crie um programa que leia duas palavras e verifique se a segunda palavra é um anagrama da primeira */
+
+let primeiraPalavra = 'isalp'.toLowerCase()
+let segundaPalavra = 'lapis'.toLowerCase()
+
+if(primeiraPalavra.length === segundaPalavra.length) {
+    for(let i = 0; i <= segundaPalavra.length; i++) {
+        if(primeiraPalavra.includes(segundaPalavra[i])) {
+            primeiraPalavra = primeiraPalavra.replace(segundaPalavra[i], '')
+        }
+    }
+}
+
+if(primeiraPalavra.length === 0) {
+    console.log('Essas palavras são anagrama uma da outra')
+} else {
+    console.log('Essas palavras não são anagrama uma da outra')
+}
