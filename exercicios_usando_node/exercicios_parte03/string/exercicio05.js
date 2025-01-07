a5cc6b10cf59b90eeeca3b6d5cfe40c5a50e4923
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-01-07 00:07:31 -0300

    exercício 05 concluído

--- exercicios_usando_node/exercicios_parte03/string/exercicio05.js
+++ exercicios_usando_node/exercicios_parte03/string/exercicio05.js
@@ -1,1 +1,25 @@
 /*Faça um programa que leia uma palavra e verifique se a mesma é palíndromo (se pode ser lida da mesma forma de trás para frente) */
+
+const nome = 'bala'
+
+//1ª forma de fazer
+const nomeInvertido = nome.split('').reverse().join('')
+
+if(nome === nomeInvertido) {
+    console.log('Essa palavra é um palíndromo')
+} else {
+    console.log('Essa palavra não é um palíndromo')
+}
+
+//2ª forma de fazer
+let novoNome = ''
+
+for(let i = nome.length; i > 0; i--) {
+    novoNome += nome[i - 1]
+}
+
+if(nome === novoNome) {
+    console.log('Essa palavra é um palíndromo')
+} else {
+    console.log('Essa palavra não é um palíndromo')
+}
