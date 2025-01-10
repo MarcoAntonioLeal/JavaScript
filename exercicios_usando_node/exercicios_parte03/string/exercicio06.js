cf50c020764a1245594ecf1685b5476e725af584
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-01-10 00:07:09 -0300

    exercício 06 concluído

--- exercicios_usando_node/exercicios_parte03/string/exercicio06.js
+++ exercicios_usando_node/exercicios_parte03/string/exercicio06.js
@@ -3,6 +3,7 @@
 let primeiraPalavra = 'isalp'.toLowerCase()
 let segundaPalavra = 'lapis'.toLowerCase()
 
+//1ª forma de fazer
 if(primeiraPalavra.length === segundaPalavra.length) {
     for(let i = 0; i <= segundaPalavra.length; i++) {
         if(primeiraPalavra.includes(segundaPalavra[i])) {
@@ -12,6 +13,20 @@ if(primeiraPalavra.length === segundaPalavra.length) {
 }
 
 if(primeiraPalavra.length === 0) {
+    console.log('Essas palavras são anagrama uma da outra')
+} else {
+    console.log('Essas palavras não são anagrama uma da outra')
+}
+
+//2ª forma de fazer
+
+const primeira = 'isalp'.toLowerCase()
+const segunda = 'lapys'.toLowerCase()
+
+const primeiroArray = primeira.split('').sort().join('')
+const segundoArray = segunda.split('').sort().join('')
+
+if(primeiroArray === segundoArray) {
     console.log('Essas palavras são anagrama uma da outra')
 } else {
     console.log('Essas palavras não são anagrama uma da outra')
