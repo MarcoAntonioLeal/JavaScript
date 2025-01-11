c190e9a4e86b953b5074fd257ae4e73e02c78373
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-01-11 11:55:27 -0300

    exercício 07 concluído

--- exercicios_usando_node/exercicios_parte03/string/exercicio07.js
+++ exercicios_usando_node/exercicios_parte03/string/exercicio07.js
@@ -1,1 +1,19 @@
 /*Escreva um programa que receba um nome completo e exiba somente o primeiro nome */
+
+const nome = 'Marco Antonio Stanziola Leal Filho'
+
+//1ª forma de fazer
+console.log(nome.split(' ')[0])
+
+//2ª forma de fazer
+let primeiroNome = ''
+
+for(let i = 0; i < nome.length; i++) {
+    if(nome[i] != ' ') {
+        primeiroNome += nome[i]
+    } else {
+        break
+    }
+}
+
+console.log(primeiroNome)
