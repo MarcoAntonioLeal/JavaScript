fa9a7b5fc40d1f7246d8e15a65f7c9da7e043ffc
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-01-11 15:30:24 -0300

    atualizando exercícios

--- exercicios_usando_node/exercicios_parte03/string/exercicio08.js
+++ exercicios_usando_node/exercicios_parte03/string/exercicio08.js
@@ -1,1 +1,19 @@
 /*Faça um programa que receba uma frase e exiba a quantidade de espaços em branco presentes na mesma */
+
+const frase = 'Olá mundo, boa tarde! Estou fazendo o exercício 08'
+
+//1ª forma de fazer
+let quantEspacos = 0
+
+for(let i = 0; i < frase.length; i++) {
+    if(frase[i] === ' ') {
+        quantEspacos += 1
+    }
+}
+
+console.log(quantEspacos)
+
+//2ª forma de fazer 
+const arrayDeEspacos = frase.match(/\s/g)
+
+console.log(arrayDeEspacos.length)
--- exercicios_usando_node/exercicios_parte03/string/exercicio09.js
+++ exercicios_usando_node/exercicios_parte03/string/exercicio09.js
@@ -1,1 +1,8 @@
 /*Crie um programa que leia uma palavra e exiba a quantidade de vogais presentes na mesma */
+
+const frase = 'Olá mundo, boa tarde! Estou fazendo o exercício 08'
+
+//1ª forma de fazer
+for(let i = 0; i < frase.length; i++) {
+
+}
