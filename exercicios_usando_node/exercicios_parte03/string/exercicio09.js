e4e3509c4d4dab67e1c86ddd741ba2ca607d0e3c
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-01-13 22:37:42 -0300

    exercício 09 concluído

--- exercicios_usando_node/exercicios_parte03/string/exercicio09.js
+++ exercicios_usando_node/exercicios_parte03/string/exercicio09.js
@@ -1,8 +1,25 @@
 /*Crie um programa que leia uma palavra e exiba a quantidade de vogais presentes na mesma */
 
-const frase = 'Olá mundo, boa tarde! Estou fazendo o exercício 08'
+const frase = 'Ola mundo, boa tarde! Estou fazendo o exercicio 08'.toLowerCase()
 
-//1ª forma de fazer
+
+let quantVogais = 0
+
 for(let i = 0; i < frase.length; i++) {
-
+    if(frase[i] === 'a' | frase[i] === 'e' | frase[i] === 'i' | frase[i] === 'o' | frase[i] === 'u') {
+        quantVogais += 1
+    }
 }
+
+console.log(quantVogais)
+
+//2ª forma de fazer
+const vogais = ['a', 'e', 'i', 'o', 'u']
+
+for(let i = 0; i < frase.length; i++) {
+    if(frase[i].includes(vogais)) {
+        quantVogais += 1
+    }
+}
+
+console.log(quantVogais)
