4e425d3a3a4f460bf1f07bfe055470bb3486445c
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-02-04 00:04:14 -0300

    atualizando exercícios

--- exercicios_usando_node/exercicios_parte03/condicionais/exercicio11.js
+++ exercicios_usando_node/exercicios_parte03/condicionais/exercicio11.js
@@ -1,1 +1,17 @@
 /*Faça um programa que leia a idade de três pessoas e informe se alguma delas é maior de idade (18 anos) */
+
+function maiorDeIdade(idade01, idade02, idade03) {
+    const idade = [idade01, idade02, idade03]
+    const maioridade = 18
+
+    idade.forEach(num => {
+        if(num >= maioridade) {
+            console.log(`Você tem ${num} anos, portanto é maior de idade`)
+        }
+        else {
+            console.log('Nenhum de vocês é amior de idade')
+        }
+    })
+}
+
+maiorDeIdade(10, 25, 38)
--- exercicios_usando_node/exercicios_parte04/ex01.js
+++ exercicios_usando_node/exercicios_parte04/ex01.js
@@ -1,25 +1,16 @@
 /*Em um bingo eu tenho uma cartela com os números 11, 28, 35, 74, 26 e 17. Desenvolva uma função que sorteie números aleatórios entre 1 e 75 até serem sorteados todos os números da cartela. Após sortear toda a cartela exibe no console a frase "Parabéns, você ganhou!" */
 
 const cartelaBingo = [11, 28, 35, 26, 17]
-let variavelDeControle = ''
-let variavel = 0
-const sorteio = (Math.random() * 74 + 1).toFixed(0)
+function sorteio() {return (Math.random() * 74 + 1).toFixed(0)}
+variavelDeControle = 0
 
-do {
-    variavelDeControle = sorteio
-    cartelaBingo.forEach((num, index) => {
-        if(num == variavelDeControle) {
-            variavel += 1
-            variavelDeControle = ''
-        } else {
-            variavelDeControle = ''
-        }
-    })
-
-    if(variavel == cartelaBingo.length) {
-        console.log('Consegui!')
+while(variavelDeControle != cartelaBingo.length) {
+        if(cartelaBingo.filter(num => num == sorteio())) {
+        variavelDeControle += 1
     }
-} while(variavel != cartelaBingo.length)
+}
 
-console.log(sorteio)
+
+
+console.log(variavelDeControle)
 console.log(cartelaBingo)
