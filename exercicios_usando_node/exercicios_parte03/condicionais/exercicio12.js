ecaf10ff3e4a76069204127908088d5b0d888034
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-02-04 21:27:54 -0300

    atualizando exercícios

--- exercicios_usando_node/exercicios_parte03/condicionais/exercicio12.js
+++ exercicios_usando_node/exercicios_parte03/condicionais/exercicio12.js
@@ -1,1 +1,11 @@
 /*Faça um programa que leia três números e informe se eles podem ser os lados de um triângulo (a soma de dois lados deve ser sempre maior que o terceiro lado) */
+
+function ladosTriangulo(lado1, lado2, lado3) {
+    if((lado1 + lado2) > lado3 && (lado1 + lado3) > lado2 && (lado2 + lado3) > lado1) {
+        console.log('Esses lados formam um triângulo')
+    } else {
+        console.log('Esses lados não formam um triângulo')
+    }
+}
+
+ladosTriangulo(30, 15, 20)
--- exercicios_usando_node/exercicios_parte03/condicionais/exercicio13.js
+++ exercicios_usando_node/exercicios_parte03/condicionais/exercicio13.js
@@ -1,1 +1,13 @@
 /*Faça um programa que leia o ano de nascimento de uma pessoa e informe se ela está apta a votar (idade maior ou igual a 16 anos) */
+
+function idadeAptaVotar(anoNascimento) {
+    const idade = new Date().getFullYear() - anoNascimento
+
+    if(idade >= 16) {
+        console.log('Você tem idade para votar')
+    } else {
+        console.log('Você não tem idade para votar')
+    }
+}
+
+idadeAptaVotar(2010)
