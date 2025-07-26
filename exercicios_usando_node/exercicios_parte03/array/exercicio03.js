e34c3ec019a06815dbd0b29835ba5a69913525db
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-07-26 12:02:18 -0300

    exercício 03 concluído

--- exercicios_usando_node/exercicios_parte03/array/exercicio03.js
+++ exercicios_usando_node/exercicios_parte03/array/exercicio03.js
@@ -1,3 +1,13 @@
 /*03) Faça um programa que leia um vetor de números inteiros e exiba o menor elemento presente no vetor*/
 
 const arrayExercicio03 = [5, 6, 8, 7, 5, 1, 10, 3, 66, 10];
+
+let menorValor = arrayExercicio03[0]
+
+for(let i = 0; i < arrayExercicio03.length; i++) {
+    if(menorValor > arrayExercicio03[i]) {
+        menorValor = arrayExercicio03[i]
+    }
+}
+
+console.log(menorValor)
