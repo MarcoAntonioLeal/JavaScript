02e69dd764ff9a1f274912825ceebd5627e407cd
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-07-26 12:50:29 -0300

    atualizando exercício 5

--- exercicios_usando_node/exercicios_parte03/array/exercicio05.js
+++ exercicios_usando_node/exercicios_parte03/array/exercicio05.js
@@ -1,1 +1,13 @@
 /*05) Crie um programa que leia dois vetores de números inteiros com o mesmo tamanho e exiba um novo vetor com a soma dos elementos correspondentes dos dois vetores.*/
+
+const primeiroArray = [5, 6, 8, 7, 5, 1];
+const segundoArray = [2, 4, 1, 20, 35, 22];
+
+//1ª forma de fazer
+let somaPrimeiroArray = 0
+let somaSegundoArray = 0
+let novoArray = []
+let somaNovoArray = 0
+
+
+//2ª forma de fazer
