009c4be9d93c1caf7a28cb451f3f3287ece8aaca
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-08-03 21:59:03 -0300

    exercício 06 concluído

--- exercicios_usando_node/exercicios_parte03/array/exercicio06.js
+++ exercicios_usando_node/exercicios_parte03/array/exercicio06.js
@@ -1,2 +1,5 @@
 /*06) Faça um programa que leia um vetor de números inteiros e verifique se ele está em ordem crescente.*/
 
+const array = [2, 4, 100, 6, 35, 22];
+
+console.log(array.sort(((a, b) => a - b)))
